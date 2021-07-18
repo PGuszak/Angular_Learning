2ec52b75dbc2271d06fea957d9ac79e5a0212daa
Paul Guszak <pguszak@yahoo.com>
2021-07-18 06:06:41 -0400

    Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'; //this is what allows me to use the submit in the addtask as a form. 
 // because I added it I need to put FormsModule in the imports section
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -11,6 +12,13 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes: Routes = [ //where componetns get added to their routes
+  {path: "", component: TasksComponent},
+  {path: "about", component: AboutComponent} //alway capitalize the component first letter???
+]
 
 @NgModule({
   declarations: [
@@ -19,13 +27,16 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,7 @@ export class HeaderComponent implements OnInit {
   showAddTask:boolean = false;
   subscription!: Subscription;
 
-  constructor(private uiService:UiService) {
+  constructor(private uiService:UiService, private router:Router) {
     this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
     //this componeents subscription var is our sucscription to our observable to our uiservice component 
     // onToggle function and settings our variable of "showAddTask" to our value we are taking in
@@ -24,6 +25,10 @@ export class HeaderComponent implements OnInit {
   toggleAddTask() {
     // console.log("toggle here"); //to make sure the html calls the right function before moving forward drilling down
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route:string) {
+    return this.router.url === route; //will return a true or false value
   }
 
 }
